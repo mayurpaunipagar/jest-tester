28005defb3ebb8521d724404972004ce9fd7e28a
Mayur Paunipagar <mayur.paunipagar@homeserveusa.com>
2025-02-26 16:47:32 +0530

    files modified

--- sum.test.js
+++ sum.test.js
@@ -19,3 +19,43 @@ test('add positive numbers is not zero',()=>{
         }
     }
 })
+
+test('null',()=>{
+    const n1 = null;
+    expect(n1).toBeNull();
+    expect(n1).toBeDefined();
+    const n2 = undefined;
+    expect(n2).toBeUndefined();
+
+    const n3 = 1===1
+    expect(n3).toBeTruthy();
+    expect(n2).toBeFalsy();
+
+})
+
+test('null',()=>{
+    const n = null;
+    expect(n).toBeNull();
+    expect(n).toBeDefined();
+    expect(n).not.toBeUndefined();
+    expect(n).not.toBeTruthy();
+    expect(n).toBeFalsy();
+
+})
+
+test('zero',()=>{
+    const z=0;
+    expect(z).not.toBeNull();
+    expect(z).toBeDefined();
+    expect(z).not.toBeUndefined();
+    expect(z).not.toBeTruthy();
+    expect(z).toBeFalsy();
+})
+
+test("greaterThanAndLessThan",()=>{
+    const value= 2+2;
+    expect(value).toBeGreaterThan(3);
+    expect(value).toBeGreaterThanOrEqual(3.5);
+    expect(value).toBeLessThan(5);
+    expect(value).toBeLessThanOrEqual(4);
+})
