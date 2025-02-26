963b2536897fdf2e22710e7c4d40c7c7583c7c14
Mayur Paunipagar <mayur.paunipagar@homeserveusa.com>
2025-02-26 17:38:46 +0530

    floating point number test case added

--- sum.test.js
+++ sum.test.js
@@ -9,31 +9,31 @@ test('object assignment', () => {
         one: 1
     }
     data['two'] = 2
-    expect(data).toEqual({one:1,two:2});
+    expect(data).toEqual({ one: 1, two: 2 });
 });
 
-test('add positive numbers is not zero',()=>{
-    for(let a=1;a<10;a++){
-        for(let b=1;b<10;b++){
-            expect(a+b).not.toBe(0);
+test('add positive numbers is not zero', () => {
+    for (let a = 1; a < 10; a++) {
+        for (let b = 1; b < 10; b++) {
+            expect(a + b).not.toBe(0);
         }
     }
 })
 
-test('null',()=>{
+test('null', () => {
     const n1 = null;
     expect(n1).toBeNull();
     expect(n1).toBeDefined();
     const n2 = undefined;
     expect(n2).toBeUndefined();
 
-    const n3 = 1===1
+    const n3 = 1 === 1
     expect(n3).toBeTruthy();
     expect(n2).toBeFalsy();
 
 })
 
-test('null',()=>{
+test('null', () => {
     const n = null;
     expect(n).toBeNull();
     expect(n).toBeDefined();
@@ -43,8 +43,8 @@ test('null',()=>{
 
 })
 
-test('zero',()=>{
-    const z=0;
+test('zero', () => {
+    const z = 0;
     expect(z).not.toBeNull();
     expect(z).toBeDefined();
     expect(z).not.toBeUndefined();
@@ -52,10 +52,22 @@ test('zero',()=>{
     expect(z).toBeFalsy();
 })
 
-test("greaterThanAndLessThan",()=>{
-    const value= 2+2;
+test("greaterThanAndLessThan", () => {
+    const value = 2 + 2;
     expect(value).toBeGreaterThan(3);
     expect(value).toBeGreaterThanOrEqual(3.5);
     expect(value).toBeLessThan(5);
     expect(value).toBeLessThanOrEqual(4);
+    // toBe and toEqual are equivalent for numbers
+    expect(value).toBe(4);
+    expect(value).toEqual(4);
 })
+
+test("adding floating point numbers", () => {
+    const value = 0.1 + 0.2;
+    // expect(value).toBe(0.3); this will not work because of rounding error
+    expect(value).toBeCloseTo(0.3); // this will work
+
+
+})
+
